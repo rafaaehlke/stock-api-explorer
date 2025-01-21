91eb928f5945cda609d12d3227faea2f31113bad
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-20 22:47:02 -0300

    :construction: Criado enum para autenticaçao de usuario

--- src/database/knex/migrations/20230811164833_createUsers.js
+++ src/database/knex/migrations/20230811164833_createUsers.js
@@ -4,6 +4,12 @@ exports.up = knex => knex.schema.createTable("users", table => {
   table.text("email").notNullable();
   table.text("password").notNullable();
 
+  // enum cria restriçoes, primeiro parametro nome que queremos para a coluna
+  // dentro do array, colocamos quais as opcoes que a coluna irá ter
+  // enumName é o nome das restricoes que ira criar
+  table.enum("role", ["admin", "customer"], { useNative: true, enumName: "roles" })
+  .notNullable.default("customer")
+
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
 });
