a8d1ec361bf42dfb1451d5bccb94a2c9a99e5b5c
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-29 00:25:55 -0300

    :construction: Criado nova role, SALE (ATIVADA NA DB)

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -2,12 +2,12 @@ const { Router } = require("express");
 const SalesController = require("../controllers/SalesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const verifyUserAuthorization = require('../middlewares/verifyUserAuthorization')
-const salesRoutes = Router();
 
+const salesRoutes = Router();
 const salesController = new SalesController();
 
 salesRoutes.use(ensureAuthenticated);
-salesRoutes.use(verifyUserAuthorization('admin'))
+salesRoutes.use(verifyUserAuthorization(["admin", "sale"]));
 
 salesRoutes.get("/", salesController.index);
 
